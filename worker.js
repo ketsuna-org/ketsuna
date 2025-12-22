c383beea5ddd5edc211799fd1345fa2e71701c4e
garder500 <jeremy27.clara22@gmail.com>
2025-12-22 23:36:15 +0100

    feat: migrer vers l'adaptateur Cloudflare et mettre à jour la configuration du worker

--- worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-    async fetch(request, env, ctx) {
-        // Servez les fichiers statiques générés par SvelteKit (adapter-static)
-        // via la binding ASSETS fournie par Wrangler.
-        return env.ASSETS.fetch(request);
-    }
-};
